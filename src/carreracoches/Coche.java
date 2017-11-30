bbcf3481fc3c2b39548773b11f772655d441a15f
Jesús <jesusruiver@jesusruiver.com>
2017-11-30 12:40:40 +0100

    Ejercicio Carrera coches

---
+++ src/carreracoches/Coche.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carreracoches;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author usuario
+ */
+public class Coche implements Runnable {
+
+    private GuiCarreraCoches carrera;
+    private int numeroCoche;
+    private String mensaje;
+    private int tiempo;
+
+    public Coche(GuiCarreraCoches carrera, int numeroCoche, String mensaje, int tiempo) {
+        this.carrera = carrera;
+        this.numeroCoche = numeroCoche;
+        this.mensaje = mensaje;
+        this.tiempo = tiempo;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 20; i++) {
+                carrera.setMensaje(numeroCoche, mensaje + " " + i);
+
+                Thread.sleep(tiempo);
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Coche.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
